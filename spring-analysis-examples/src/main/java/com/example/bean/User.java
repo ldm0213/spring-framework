081196549ea156787bfe7b37ac8f5443eaeb9a78
lidongmeng <lidongmeng@kuaishou.com>
2022-02-18 16:32:04 +0800

    Add:add get bean module

---
+++ spring-analysis-examples/src/main/java/com/example/bean/User.java
@@ -0,0 +1,34 @@
+package com.example.bean;
+
+/**
+ * @author lidongmeng
+ * Created on 2022-02-18
+ */
+public class User {
+	private String username;
+	private String address;
+
+	@Override
+	public String toString() {
+		return "User{" +
+				"username='" + username + '\'' +
+				", address='" + address + '\'' +
+				'}';
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
